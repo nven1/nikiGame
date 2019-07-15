e4a4d5d91fb6f9d11e548401bb0343933851e471
nven1 <neven.zdelar@gmail.com>
2019-07-15 02:51:53 +0200

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,71 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'niki';
+  columns = [0,1,2];
+  targetTile;
+  biggest;
+
+
+  objects = [
+    {
+      size: 1,
+      column: 1
+    },
+    {
+      size: 2,
+      column: 1
+    },
+    {
+      size: 3,
+      column: 1
+    },
+    {
+      size: 4,
+      column: 1
+    },
+    {
+      size: 5,
+      column: 1
+    },
+    {
+      size: 6,
+      column: 1
+    },
+    {
+      size: 7,
+      column: 1
+    },
+    {
+      size: 8,
+      column: 1
+    },
+    {
+      size: 9,
+      column: 1
+    },
+  ];
+
+selectStartColumn(col) {
+  if (this.targetTile == undefined) {
+    for(let i=0; i<this.objects.length; i++) {
+      //console.log('col: '+col+' col2:'+this.objects[i].column);
+      if (this.objects[i].column == col) {
+        this.targetTile = i;
+        console.log(this.targetTile);
+        break;
+      }
+    }
+  }
+  else if (this.targetTile != undefined) {
+    for(let i=0; i<this.objects.length; i++) {
+      if(this.objects[i].column == col) {
+        if (this.targetTile<i) {
+          this.objects[this.targetTile].column=col;
+          this.targetTile = undefined;
+        }
+      }
+    }
+  }
+}
+
 }
